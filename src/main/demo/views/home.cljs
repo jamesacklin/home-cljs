811ceb5e8d108a408913b95198d51eca88677f64
James Acklin <james.acklin@gmail.com>
2019-09-13 22:52:15 -0400

    let's update this

--- src/main/demo/views/home.cljs
+++ src/main/demo/views/home.cljs
@@ -1,6 +1,6 @@
 (ns demo.views.home
   (:require [re-frame.core :as r]
-            [stylefy.core :as s] 
+            [stylefy.core :as s]
             [demo.views.page :refer [page-view]]))
 
 (def inline-li {:display     "inline"
@@ -29,15 +29,15 @@
    {:content
     [:div
      [title]
-     [:p "I’m James Acklin, a "[:span {:title "I have not yet designed a chair"} "digital product designer"]" and front-end web developer focused on usability, prototyping, and distributed design systems."]
-     [:p "I work full-time for " [:em "Nielsen Media Analytics"] [:sup "*"] " in Pittsburgh, PA. I’ve created digital experiences and tools on the Web for startups, large agencies, and Fortune 500 companies" [:sup "†"] " for about a decade."]
-     [:p "My design practice concentrates on human-centered design methodologies and high-fidelity prototyping with an emphasis on in-browser deliverables. I have a passion for manipulating data and working with the raw material of the web." [:sup "‡"]]
+     [:p "I am James Acklin, a " [:span {:title "I have not yet designed a chair"} "digital product designer"] " and front-end web developer focused on usability, prototyping, and distributed design systems."]
+     [:p "I work full-time for " [:em "Nielsen Media Analytics"] [:sup "*"] " in Pittsburgh, PA. I have created digital experiences and tools on the Web for startups, large agencies, and Fortune 500 companies" [:sup "†"] " for about a decade."]
+     [:p "I am comfortable in pro-complexity UX vacuums and have extensive experience articulating the business value of human-centered design. My design practice concentrates on high-fidelity prototyping, information design, and living code deliverables. I have a passion for manipulating data and working with the raw material of the web." [:sup "‡"]]
      [:p "Occasionally I conduct branding exercises, help software companies and websites with interaction design problems, and do general graphic design work on the side. I am not currently accepting freelance engagements."]
      [:div {:id "colophon"}
       [:ul
        [:li (s/use-style (merge colophon-li {::s/mode {:before {:content "'*'"}}}))
-        "At Nielsen, I research, prototype, design, and contribute production interface code for all user-facing features of Rhiza, a web-based data visualization tool used by Media Analytics to perform geospatial analysis and publish client-facing instruments. I also administer the Rhiza Design System, a standardized front-end component library designed for re-use by application developers within Media Analytics and beyond."]
+        "My current role is at the intersection between product and engineering for Media Analytics, where I work with stakeholders and users in early concept validation and systems-based problem-solving through Agile processes. I use design activities and exercises to develop personas, define requirements, and design interaction frameworks. I also maintain a visual design system and front-end component library using ClojureScript and React for quick project scaffolding and consistent user experiences."]
        [:li (s/use-style (merge colophon-li {::s/mode {:before {:content "'†'"}}}))
-        "I have worked with consumer packaged goods brands (the family of Nestlé ice cream brands, Del Monte foods, CLIF, Premier Protein), academic institutions and non-profits (The Winchester Thurston School, The Sarah Heinz House, The String Orchestra of Brooklyn), software vendors (VIA Oncology, Management Science Associates), retailers (Avalon Exchange, rue21, JOAN Boutique), restaurants (" [:a {:href "https://primantibros.com/"}"Primanti Brothers"] ", Big Burrito), industrial and B2B concerns (HEICO Fasteners, HarbisonWalker International), and cycling (Aspire Racing/Jeremy Powers, the JAM Fund, " [:a {:href "https://github.com/jamesacklin/send"} "Dirt Rag Magazine"] ", " [:a {:href "https://github.com/jamesacklin/ssc"} "Steady State Cycles"] ")."]
+        "I have worked with consumer packaged goods brands (the family of Nestlé ice cream brands, Del Monte foods, CLIF, Premier Protein), academic institutions and non-profits (The Winchester Thurston School, The Sarah Heinz House, The String Orchestra of Brooklyn), software vendors (VIA Oncology, Management Science Associates), retailers (Avalon Exchange, rue21, JOAN Boutique), restaurants (" [:a {:href "https://primantibros.com/"} "Primanti Brothers"] ", Big Burrito), industrial and B2B concerns (HEICO Fasteners, HarbisonWalker International), and cycling (Aspire Racing/Jeremy Powers, the JAM Fund, " [:a {:href "https://github.com/jamesacklin/send"} "Dirt Rag Magazine"] ", " [:a {:href "https://github.com/jamesacklin/ssc"} "Steady State Cycles"] ")."]
        [:li (s/use-style (merge colophon-li {::s/mode {:before {:content "'‡'"}}}))
         "This site is written in ClojureScript using Reagent, re-frame and stylefy, compiled with shadow-cljs, and served via ▴Now. " [:a {:href "https://github.com/jamesacklin/home-cljs"} "Its source is available on GitHub."] " The type and background treatments are inspired by the cover art for the 2017 Ben Frost album " [:em "The Centre Cannot Hold"] "."]]]]}])
--- src/main/demo/views/work.cljs
+++ src/main/demo/views/work.cljs
@@ -13,12 +13,13 @@
 
 (defn work []
   [page-view
-   {:content 
+   {:content
     [:div
      [:h1 (s/use-style {:font-size "10vw"}) "Work"]
-     [:p "For the past 5 years, I have been the primary designer (and a contributing front-end developer) for Rhiza, a data research tool and analytics startup spun off from MAYA Design in 2007 and acquired by The Nielsen Company in 2017. I have led innovation in our user interface through primary research, concepting, prototyping, validation, detailed visual design, and front-end development."]
+     [:p "For the past ~6 years, I have been the primary designer (and a contributing front-end developer) for Rhiza, a data research tool and analytics startup spun off from MAYA Design in 2007 and acquired by The Nielsen Company in 2017. I have led innovation in our user interface through primary research, concepting, prototyping, validation, detailed visual design, and front-end development."]
      [:p "In short, Rhiza is a B2B data visualization product used by many teams at Nielsen. It is designed to mask its own complexity in order to deliver simple, actionable results, while simultaneously providing great expressive power for data storytellers and researchers."]
-     [:p "I’d be happy to show you what Rhiza looks like, but I’m still working on exactly how to communicate its constant user-centric evolution. Instead, here’s a folder listing of the single-player dungeon crawler that has been the past 5 years of my career. The folders are ordered by year, then month. Detailed timeline and case studies forthcoming."]
+     [:p "I have also worked on various early-stage product concepts and lent a UXer’s hand to product teams in need of usability enhancements or wrangling their visual design system."]
+     [:p "I’d be happy to show you what my work for Nielsen looks like, but I’m still working on exactly how to communicate its constant user-centric evolution. Instead, here’s a folder listing of the single-player dungeon crawler that has been the past 6 years of my career. The folders are ordered by year, then month. Detailed timeline and case studies forthcoming."]
      [:div (s/use-style (merge (big-work)
                                {:transition "all 0.2s ease"})
                         {:on-click #(r/dispatch [:expand-work])})
